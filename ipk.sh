e2ef5e34c2c08ae879a4f0567d570acfe4e4ec31
Netflixxp <61532011+Netflixxp@users.noreply.github.com>
2023-01-13 16:01:35 +0800

    Update ipk.sh

--- ipk.sh
+++ ipk.sh
@@ -21,7 +21,7 @@ svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-passwall2 pack
 # svn co https://github.com/xiaorouji/openwrt-passwall2/trunk/luci-app-passwall2 package/luci-app-passwall2
 svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/luci-app-ssr-plus
 svn co https://github.com/xiaorouji/openwrt-passwall/trunk/sagernet-core package/sagernet-core
-svn co https://github.com/xiaorouji/luci-app-bypass/trunk/luci-app-bypass package/luci-app-bypass
+svn co https://github.com/kenzok8/luci-app-bypass/trunk/luci-app-bypass package/luci-app-bypass
 
 # sagernet-core
 sed -i 's|$(LN) v2ray $(1)/usr/bin/xray|#$(LN) v2ray $(1)/usr/bin/xray|g' package/sagernet-core/Makefile
