ca3d2bbecfb4b92d903aac4e8299f105d277b6ef
Netflixxp <61532011+Netflixxp@users.noreply.github.com>
2023-01-13 16:05:20 +0800

    Update ipk.sh

--- ipk.sh
+++ ipk.sh
@@ -17,8 +17,8 @@ svn co https://github.com/281677160/openwrt-package/trunk/luci-app-smartdns
 # 科学上网插件依赖
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
 svn co https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall package/luci-app-passwall
-svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-passwall2 package/luci-app-passwall2
-# svn co https://github.com/xiaorouji/openwrt-passwall2/trunk/luci-app-passwall2 package/luci-app-passwall2
+#svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-passwall2 package/luci-app-passwall2
+svn co https://github.com/xiaorouji/openwrt-passwall2/tree/main/luci-app-passwall2 package/luci-app-passwall2
 svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/luci-app-ssr-plus
 svn co https://github.com/xiaorouji/openwrt-passwall/trunk/sagernet-core package/sagernet-core
 svn co https://github.com/kenzok8/luci-app-bypass/trunk/luci-app-bypass package/luci-app-bypass
